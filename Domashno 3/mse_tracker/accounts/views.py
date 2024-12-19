7918841a2e3e55ba2d8a6e569bc502d9239defee
Nikola <nikolatodorov623@gmail.com>
2024-12-19 17:31:56 +0100

    Login najava na korisnici.

--- Domashno 3/mse_tracker/accounts/views.py
+++ Domashno 3/mse_tracker/accounts/views.py
@@ -6,21 +6,24 @@ from rest_framework.response import Response
 from rest_framework import status
 from django.contrib.auth import login
 
-
 class LoginView(APIView):
+    permission_classes = [AllowAny]
 
     def post(self, request):
-        serializer = LoginSerializer(data=request.data)
-        if serializer.is_valid():
-            user = authenticate(
-                username=serializer.validated_data['username'],
-                password=serializer.validated_data['password']
-            )
-            if user:
-                login(request, user)
-                return Response({"message": "Login successful"}, status=status.HTTP_200_OK)
+
+        username = request.data.get("username")
+        password = request.data.get("password")
+
+
+        user = authenticate(username=username, password=password)
+
+        if user is not None:
+            #logiranje na korisnikot
+            login(request, user)
+            return Response({"message": "Login successful"}, status=status.HTTP_200_OK)
+        else:
             return Response({"error": "Invalid credentials"}, status=status.HTTP_401_UNAUTHORIZED)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
 
 
 class LogoutView(APIView):
@@ -43,6 +46,6 @@ class RegisterView(APIView):
         serializer = RegisterSerializer(data=request.data)
         if serializer.is_valid():
             user = serializer.save()
-            login(request, user)  # Log the user in after successful registration
+            login(request, user)
             return Response({"message": "User registered and logged in successfully"}, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
--- Domashno 3/mse_tracker/mse_fin_tracker/settings.py
+++ Domashno 3/mse_tracker/mse_fin_tracker/settings.py
@@ -146,3 +146,7 @@ STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 CORS_ALLOW_ALL_ORIGINS = True
+CORS_ALLOW_CREDENTIALS = True
+CORS_ORIGIN_WHITELIST = [
+    "http://localhost:3000",
+]
